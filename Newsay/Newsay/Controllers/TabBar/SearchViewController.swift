490211fcd859d8b3d565414ccf2b1af5fe67d58d
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-01-29 15:34:22 +0300

    Create tabbar controller

--- Newsay/Newsay/Controllers/TabBar/SearchViewController.swift
+++ Newsay/Newsay/Controllers/TabBar/SearchViewController.swift
@@ -1,29 +1,10 @@
-//
-//  SearchViewController.swift
-//  Newsay
-//
-//  Created by Oğuzhan Kaymak on 29.01.2022.
-//
-
 import UIKit
 
 class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Newsay/Newsay/Controllers/TabBar/TabBarViewController.swift
+++ Newsay/Newsay/Controllers/TabBar/TabBarViewController.swift
@@ -1,10 +1,3 @@
-//
-//  TabBarViewController.swift
-//  Newsay
-//
-//  Created by Oğuzhan Kaymak on 29.01.2022.
-//
-
 import UIKit
 
 class TabBarViewController: UITabBarController {
@@ -12,18 +5,31 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let vc1 = HomeViewController()
+        let vc2 = CategoryViewController()
+        let vc3 = SearchViewController()
+        
+        vc1.title = "News"
+        vc2.title = "Category"
+        vc3.title = "Search"
+        
+        vc2.navigationItem.largeTitleDisplayMode = .always
+        vc3.navigationItem.largeTitleDisplayMode = .always
+        
+        let nav1 = UINavigationController(rootViewController: vc1)
+        let nav2 = UINavigationController(rootViewController: vc2)
+        let nav3 = UINavigationController(rootViewController: vc3)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Category", image: UIImage(systemName: "list.bullet"), tag: 1)
+        nav3.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        
+        nav1.navigationBar.scrollEdgeAppearance = nav1.navigationBar.standardAppearance
+        
+        nav2.navigationBar.prefersLargeTitles = true
+        nav3.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers(([nav1,nav2, nav3]), animated: false)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
