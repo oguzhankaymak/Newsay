ac287cf37eebd58379b34f84bd02d5c0206e2b0b
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-01-29 15:34:07 +0300

    Config project default route

--- Newsay/Newsay/Resources/AppDelegate.swift
+++ Newsay/Newsay/Resources/AppDelegate.swift
@@ -1,19 +1,17 @@
-//
-//  AppDelegate.swift
-//  Newsay
-//
-//  Created by Oğuzhan Kaymak on 29.01.2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = TabBarViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
--- Newsay/Newsay/Resources/SceneDelegate.swift
+++ Newsay/Newsay/Resources/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  Newsay
-//
-//  Created by Oğuzhan Kaymak on 29.01.2022.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -13,10 +6,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = TabBarViewController()
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
