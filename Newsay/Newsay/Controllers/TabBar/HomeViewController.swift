74205e38a48463611943b0151453bf13d1f779e3
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-01-30 15:33:40 +0300

    Add get news request

--- Newsay/Newsay/Controllers/TabBar/HomeViewController.swift
+++ Newsay/Newsay/Controllers/TabBar/HomeViewController.swift
@@ -1,11 +1,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private var news: [Article] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        fetchData()
 
+    }
+    
+    private func fetchData(){
+        APICaller.shared.getNews { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let news):
+                    self?.news = news.articles
+                    
+                case.failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 
 }
--- Newsay/Newsay/Network/APICaller.swift
+++ Newsay/Newsay/Network/APICaller.swift
@@ -12,5 +12,30 @@ final class APICaller {
     enum APIError : Error {
         case failedToGetData
     }
+    
+    public func getNews(completion: @escaping (Result<NewsResponse, Error>) -> Void){
+        guard let url = URL(string: Constants.baseAPIURL) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(APIError.failedToGetData))
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(NewsResponse.self, from: data)
+                completion(.success(result))
+            }
+            
+            catch {
+                print("error.localizedDescription : \(error.localizedDescription)")
+                print("error : \(error)")
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
 }
 
