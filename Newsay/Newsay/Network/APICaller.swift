0e6dd0bfd5dd221c7fd1a3c0abd4adfac840b991
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-02-12 14:49:15 +0300

    Add get news by search

--- Newsay/Newsay/Network/APICaller.swift
+++ Newsay/Newsay/Network/APICaller.swift
@@ -63,5 +63,31 @@ final class APICaller {
         }
         task.resume()
     }
+    
+    public func getNewsBySearch(with searchText: String, completion: @escaping (Result<NewsResponse, Error>) -> Void){
+        
+        guard let url = URL(string: "\(Constants.baseAPIURL)&q=\(searchText)" ) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(APIError.failedToGetData))
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(NewsResponse.self, from: data)
+                completion(.success(result))
+            }
+            
+            catch {
+                print("error.localizedDescription : \(error.localizedDescription)")
+                print("error : \(error)")
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
 }
 
