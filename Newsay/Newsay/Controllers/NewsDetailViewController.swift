116310be6c665573f66c09397f4d049c03095bff
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-02-16 20:53:31 +0300

    Show loader when load page in webview

--- Newsay/Newsay/Controllers/NewsDetailViewController.swift
+++ Newsay/Newsay/Controllers/NewsDetailViewController.swift
@@ -16,6 +16,14 @@ class NewsDetailViewController: UIViewController, WKNavigationDelegate {
         return webView
     }()
     
+    private var activiyLoader : UIActivityIndicatorView = {
+        let activityIndicatorView = UIActivityIndicatorView()
+        activityIndicatorView.hidesWhenStopped = true
+        activityIndicatorView.style = .large
+        activityIndicatorView.color = .systemGray
+        return activityIndicatorView
+    }()
+    
     init(news: Article) {
         self.news = news
         super.init(nibName: nil, bundle: nil)
@@ -40,11 +48,23 @@ class NewsDetailViewController: UIViewController, WKNavigationDelegate {
         }
         
         webView.load(URLRequest(url: url))
+        webView.addSubview(activiyLoader)
+        activiyLoader.center = view.center
+        activiyLoader.startAnimating()
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         webView.frame = view.bounds
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activiyLoader.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activiyLoader.stopAnimating()
     }
 
 }
